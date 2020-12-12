b0c1421e1beed719fb32abb885b34022197990fa
Tiago Silva <tiago.toya@hotmail.com>
2020-12-12 17:37:57 -0300

    refactor: tratativa no nome da abilidade

--- src/resolvers/ability/ability.js
+++ src/resolvers/ability/ability.js
@@ -1,6 +1,9 @@
 const superagent = require('superagent')
 
 const name = ({ name }) => name
+  .split('-')
+  .map(world => world.charAt(0).toUpperCase() + world.slice(1))
+  .join(' ')
 
 const description = async ({ url }) => {
   const { body } = await superagent.get(url).catch(() => ({ body: '' }))
